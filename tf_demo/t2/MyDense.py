b2eda0e156b2cb7bb23a218b059eace0d05e5a62
lehaifeng000 <lehaifeng000@163.com>
2020-09-29 18:54:35 +0800

    add

--- tf_demo/t2/MyDense.py
+++ tf_demo/t2/MyDense.py
@@ -1,1 +1,24 @@
+import tensorflow as tf
+from tensorflow import keras
 
+
+class MyDense(keras.layers.Layer):
+    def __init__(self, units):
+        super().__init__()
+        self.units = units
+
+    def build(self, input_shape):
+        self.w = self.add_variable(
+            name='w',
+            shape=[input_shape[-1, self.units]],
+            initializer=tf.initializers.RandomNormal()
+        )
+        self.b = self.add_variable(
+            name='b',
+            shape=[self.units],
+            initializer=tf.initializers.Zeros()
+        )
+
+    def call(self, input):
+        return input @ self.w + self.b
+        return tf.nn.relu(input @ self.w + self.b)
