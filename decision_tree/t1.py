10290b0393a71a94a246739f45285bf95b7416dc
lehaifeng <lehaifeng000@163.com>
2020-10-20 14:05:02 +0800

    update lenet5

---
+++ decision_tree/t1.py
@@ -0,0 +1,15 @@
+# sklearn 决策树练习
+
+from sklearn import tree
+import graphviz
+x = [[0, 0], [1, 1]]
+y = [0, 1]
+clf = tree.DecisionTreeClassifier()
+
+clf.fit(x, y)
+res = clf.predict([[1.2, 1.2]])
+print(res)
+dot_data=tree.export_graphviz(clf,class_names=['属性1','属性2'])
+
+graph=graphviz.Source(dot_data)
+graph
--- tf_demo/t1/lenet5.py
+++ tf_demo/t1/lenet5.py
@@ -12,29 +12,37 @@ test_img = tf.reshape(x_test, (10000, 28, 28, 1))
 
 net = keras.models.Sequential([
     # 卷积层1
-    tf.keras.layers.Conv2D(filters=6, kernel_size=(5, 5), activation="relu", input_shape=(28, 28, 1), padding="same"),
+    tf.keras.layers.Conv2D(filters=6, kernel_size=3, activation="relu", input_shape=(28, 28, 1), padding="same"),
     tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2),
 
     # 卷积层2
-    tf.keras.layers.Conv2D(filters=16, kernel_size=(5, 5), activation="relu", padding="same"),
+    tf.keras.layers.Conv2D(filters=6, kernel_size=3, activation="relu", padding="same"),
     tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
 
     # 卷积层3
-    tf.keras.layers.Conv2D(filters=32, kernel_size=(5, 5), activation="relu", padding="same"),
+    tf.keras.layers.Conv2D(filters=16, kernel_size=3, activation="relu", padding="same"),
+    tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
+
+    # 卷积层4
+    tf.keras.layers.Conv2D(filters=16, kernel_size=3, activation="relu", padding="same"),
     # tf.keras.layers.MaxPool2D(pool_size=2,strides=2),
 
     tf.keras.layers.Flatten(),
 
     # 全连接层1
-    tf.keras.layers.Dense(200, activation="relu"),
+    tf.keras.layers.Dense(120, activation="relu"),
 
     # 全连接层2
+    tf.keras.layers.Dense(84, activation="relu"),
+
+    # 全连接层3
     tf.keras.layers.Dense(10, activation="softmax")
 
 ])
 
 net.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-net.fit(train_img, y_train, epochs=5, validation_split=0.1)
+
+net.fit(train_img, y_train, epochs=5)
 
 testLoss, testAcc = net.evaluate(test_img, y_test)
 print(testAcc)
