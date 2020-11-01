3799ac06f9371441d31ec9b840b0edd6ec23316d
lehaifeng000 <lehaifeng000@163.com>
2020-11-01 08:56:05 +0800

    add shuffle

--- tf_demo/resnet/resnet18_cifar10.py
+++ tf_demo/resnet/resnet18_cifar10.py
@@ -3,10 +3,10 @@ from tensorflow import keras
 import numpy as np
 import resnet 
 
-# gpus= tf.config.experimental.list_physical_devices('GPU')
-gpus= tf.config.list_physical_devices('GPU') # tf2.1版本该函数不再是experimental
-# print(gpus) # 前面限定了只使用GPU1(索引是从0开始的,本机有2张RTX2080显卡)
-tf.config.experimental.set_memory_growth(gpus[0], True) # 其实gpus本身就只有一个元素
+# # gpus= tf.config.experimental.list_physical_devices('GPU')
+# gpus= tf.config.list_physical_devices('GPU') # tf2.1版本该函数不再是experimental
+# # print(gpus) # 前面限定了只使用GPU1(索引是从0开始的,本机有2张RTX2080显卡)
+# tf.config.experimental.set_memory_growth(gpus[0], True) # 其实gpus本身就只有一个元素
 
 # tf.compat.v1.disable_eager_execution()
 
@@ -16,9 +16,13 @@ cifar10=keras.datasets.cifar10
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train=x_train.astype(np.float)/255.0
 x_test=x_test.astype(np.float)/255.0
+# x_train=x_train.astype(np.float32)
 print(x_train.dtype, type(x_train), x_train.shape)
-
-
+import random
+index = [i for i in range(len(x_train))] 
+random.shuffle(index)
+x_train = x_train[index]
+y_train = y_train[index]
 
 net=resnet.ResNet34()
 
